54a493a37707fb7eab852b7ea63515e1f220031f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-10 14:40:23 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	int t;
-	cin >> t;
-	while(t--) {
+	int tt;
+	cin >> tt;
+	while (tt--) {
 		int n, x;
 		cin >> n >> x;
 		char a[n];
-		for(int i=0; i<n; ++i)
+		for (int i = 0; i < n; i++) {
 			cin >> a[i];
-		int b[n], cnt=1;
-		b[0]=x;
-		for(int i=0; i<n; ++i) {
-			if(a[i]=='L')
-				x--;
-			else
-				x++;
-			b[cnt++]=x;
 		}
-		int	ans=1;
-		sort(b, b+cnt);
-		for(int i=1; i<cnt; ++i)
-			if(b[i]!=b[i-1])
+		int b[n], cnt = 1;
+		b[0] = x;
+		for (int i = 0; i < n; i++) {
+			if (a[i] == 'L') {
+				x--;
+			} else {
+				x++;
+			}
+			b[cnt++] = x;
+		}
+		int ans = 1;
+		sort(b, b + cnt);
+		for (int i = 0; i < cnt - 1; i++) {
+			if (b[i] != b[i + 1]) {
 				ans++;
-		cout << ans << "\n";
+			}
+		}
+		cout << ans << '\n';
 	}
+	return 0;
 }
