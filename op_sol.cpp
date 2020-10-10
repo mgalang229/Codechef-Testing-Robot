fd625b763ec1b29e7f6a23f081d8da3445c36879
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-10 14:42:13 +0800

    Update op_sol.cpp

--- op_sol.cpp
+++ op_sol.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	int t;
-	cin >> t;
-	while(t--) {
+	int tt;
+	cin >> tt;
+	while (tt--) {
 		int n, x;
 		cin >> n >> x;
 		char a[n];
-		for(int i=0; i<n; ++i)
+		for (int i = 0; i < n; i++) {
 			cin >> a[i];
+		}
 		set<int> s;
 		s.insert(x);
-		for(int i=0; i<n; ++i) {
-			if(a[i]=='L')
+		for (int i = 0; i < n; i++) {
+			if (a[i] == 'L') {
 				x--;
-			else
+			} else {
 				x++;
+			}
 			s.insert(x);
 		}
-		cout << (int)s.size() << "\n";
+		cout << (int) s.size() << '\n';
 	}
+	return 0;
 }
